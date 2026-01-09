f7c172f72fa896cb48bfb156fe9edb26ed6884ce
AdminAuditavel <admin@auditavel.com>
2026-01-08 21:10:31 -0300

    Delete src/components/Card.tsx

--- src/components/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Card({ children, className = '', style }: { children: React.ReactNode; className?: string; style?: React.CSSProperties }) {
-  return (
-    <div className={`card ${className}`} style={style}>
-      {children}
-    </div>
-  );
-}
