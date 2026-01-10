948cedac2e3b8a34f53a81fbf21474f426e202ff
AdminAuditavel <admin@auditavel.com>
2026-01-10 08:03:04 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 // src/App.tsx
 
 import React, { useState } from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
 import AirportHome from "./components/AirportHome";
 import VisualFeedDetail from "./components/VisualFeedDetail";
 import NotamDetail from "./components/NotamDetail";
 import WeatherModal from "./components/WeatherModal";
 
+type LocationState = {
+  background?: ReturnType<typeof useLocation>;
+};
+
 const AppContent: React.FC = () => {
   const [isWeatherModalOpen, setWeatherModalOpen] = useState(false);
+  const location = useLocation();
+
+  // Quando abrimos o detalhe "por cima", passamos state: { background: location }
+  const state = location.state as any;
+  const background = state?.background as typeof location | undefined;
 
   return (
     <div className="relative min-h-screen bg-background-dark text-white selection:bg-primary selection:text-white">
-      <Routes>
+      {/* Rotas principais (quando existir background, renderiza "por trás") */}
+      <Routes location={background || location}>
         <Route
           path="/"
           element={<AirportHome onOpenWeather={() => setWeatherModalOpen(true)} />}
@@ -20,6 +30,13 @@ const AppContent: React.FC = () => {
         <Route path="/post/:id" element={<VisualFeedDetail />} />
         <Route path="/notam/:id" element={<NotamDetail />} />
       </Routes>
+
+      {/* Rotas “modal” por cima (apenas quando background existe) */}
+      {background && (
+        <Routes>
+          <Route path="/post/:id" element={<VisualFeedDetail />} />
+        </Routes>
+      )}
 
       {isWeatherModalOpen && (
         <WeatherModal onClose={() => setWeatherModalOpen(false)} />
